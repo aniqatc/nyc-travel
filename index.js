737f02f676bc02223cd6a51b70ddc07963229dd8
aniqatc <aniqatc97@gmail.com>
2023-09-14 17:15:33 -0400

    Add destination card styling & layout + toggle map function

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ navLinks.forEach(el =>
 		navTitle.style.fontWeight = '400';
 	})
 );
+
+const mapButton = document.querySelector('.toggle-map');
+const map = document.querySelector('.destination-map');
+const destinationImg = document.querySelector(
+	'.destination-card__img-wrapper img'
+);
+
+mapButton.addEventListener('click', function () {
+	console.log('Button clicked');
+	console.log('map.style.display:', map.style.display);
+	if (map.style.display === 'none') {
+		map.style.display = 'block';
+		destinationImg.style.gridColumn = 'span 1';
+		destinationImg.style.aspectRatio = '1 / 1';
+	} else {
+		map.style.display = 'none';
+		destinationImg.style.gridColumn = 'span 2';
+		destinationImg.style.aspectRatio = '2 / 1';
+	}
+});
